113f4b7072e917551c4a72fe70a297ab700eed39
Joseph Rafferty <jrafferty@tamu.edu>
2026-01-28 20:52:13 -0600

    adds new output: external_validation_records

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,14 @@ output "deployment" {
 output "top_level_domain" {
   value = var.site_settings.top_level_domain
 }
+
+output "external_validation_records" {
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+      #} if length(regexall("${var.site_settings.top_level_domain}$", dvo.domain_name)) > 0
+    } if endswith(dvo.domain_name, ".cloud.tamu.edu") || contains(var.external_validation_list, dvo.domain_name)
+  }
+}
